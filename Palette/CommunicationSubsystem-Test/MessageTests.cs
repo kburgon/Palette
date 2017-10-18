95df9ece1c6b17846d0169ccb54ef266cd25b3b8
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2017-10-17 18:19:58 -0600

    Adds first message test

---
+++ Palette/CommunicationSubsystem-Test/MessageTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using CommunicationSubsystem.Messages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnassignMessage
+    {
+        [TestMethod]
+        public void BrushStrokeMessageTest()
+        {
+            var brushStroke = new BrushStrokeMessage();
+            brushStroke.Points = new List<Tuple<int, int>>
+            {
+                new Tuple<int, int>(1, 2),
+                new Tuple<int, int>(3, 4)
+            };
+
+            Assert.AreEqual(new Tuple<int, int>(1, 2), brushStroke.Points.First());
+        }
+    }
+}
