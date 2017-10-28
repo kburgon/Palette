f1b73a2e94c435b9a15810c3e668610728352b5a
Jason Bingham <jbingham408@gmail.com>
2017-10-27 18:09:16 -0600

    Worked on Comm subsytem core

---
+++ Palette/CommunicationSubsystem/Dispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationSubsystem
+{
+    class Dispatcher
+    {
+        protected EnvelopeQueueDictionary envelopeQueueDict = new EnvelopeQueueDictionary();
+        private EnvelopeQueue _envelopeQueue { get; set; }
+        private Envelope _envelope { get; set; }
+        private string _convId { get; set; }
+
+        public void EnqueueEnvelope()
+        {
+            GetQueue();
+            if(_envelope != null)
+                _envelopeQueue.Enqueue(_envelope);
+        }
+
+        //public Envelope DequeueEnvelope()
+        //{
+        //    GetQueue();
+        //    envelope = envelopeQueue.Dequeue();
+
+        //    return envelope;
+        //}
+
+        public void GetQueue()
+        {
+            _envelopeQueue = envelopeQueueDict.GetConversation(_convId);
+        }
+    }
+}
