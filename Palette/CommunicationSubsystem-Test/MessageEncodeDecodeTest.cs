f8693dae9ddebf03bfe26d1a06b6b27baac2248b
Jason Bingham <jbingham408@gmail.com>
2017-10-31 15:08:08 -0600

    Worked on message tests

--- Palette/CommunicationSubsystem-Test/MessageEncodeDecodeTest.cs
+++ Palette/CommunicationSubsystem-Test/MessageEncodeDecodeTest.cs
@@ -12,21 +12,34 @@ namespace UnitTestProject1
         [TestMethod]
         public void BrushStrokeMessage()
         {
-            //BrushStrokeMessage message = new BrushStrokeMessage();
-            //message.MessageNumber = new Tuple<short, short>(1, 1);
-            //message.ConversationId = new Tuple<short, short>(2, 1);
-            //message.CanvasId = 1;
-            //message.Points.Add(new Tuple<int, int>(1200, 1300));
-            //message.Points.Add(new Tuple<int, int>(1201, 1302));
-            //message.Points.Add(new Tuple<int, int>(1203, 1305));
-            //message.Points.Add(new Tuple<int, int>(1205, 1306));
-            //message.Points.Add(new Tuple<int, int>(3, 34));
-            //message.Points.Add(new Tuple<int, int>(10, 13));
-            //message.BrushType = "Solid";
+            BrushStrokeMessage message = new BrushStrokeMessage();
+            message.MessageNumber = new Tuple<short, short>(1, 1);
+            message.ConversationId = new Tuple<short, short>(2, 1);
+            message.CanvasId = 1;
+            message.Points.Add(new Tuple<int, int>(1200, 1300));
+            message.Points.Add(new Tuple<int, int>(1201, 1302));
+            message.Points.Add(new Tuple<int, int>(1203, 1305));
+            message.Points.Add(new Tuple<int, int>(1205, 1306));
+            message.Points.Add(new Tuple<int, int>(3, 34));
+            message.Points.Add(new Tuple<int, int>(10, 13));
+            message.BrushType = "Solid";
 
-            //byte[] bytes = message.Encode();
+            byte[] bytes = message.Encode();
 
-            //Assert.AreEqual(0, bytes[0]);
+            Assert.AreEqual(123, bytes[0]);
+
+            Message message2 = Message.Decode(bytes);
+
+            Assert.AreEqual(message.MessageNumber, (message2 as BrushStrokeMessage).MessageNumber);
+            Assert.AreEqual(message.ConversationId, (message2 as BrushStrokeMessage).ConversationId);
+            Assert.AreEqual(message.BrushType, (message2 as BrushStrokeMessage).BrushType);
+            Assert.AreEqual(message.CanvasId, (message2 as BrushStrokeMessage).CanvasId);
+            Assert.AreEqual(message.Points[0].Item1, (message2 as BrushStrokeMessage).Points[0].Item1);
+            Assert.AreEqual(message.Points[0].Item2, (message2 as BrushStrokeMessage).Points[0].Item2);
+            Assert.AreEqual(message.Points[2].Item1, (message2 as BrushStrokeMessage).Points[2].Item1);
+            Assert.AreEqual(message.Points[2].Item2, (message2 as BrushStrokeMessage).Points[2].Item2);
+            Assert.AreEqual(message.Points[5].Item1, (message2 as BrushStrokeMessage).Points[5].Item1);
+            Assert.AreEqual(message.Points[5].Item2, (message2 as BrushStrokeMessage).Points[5].Item2);
 
         }
         [TestMethod]
