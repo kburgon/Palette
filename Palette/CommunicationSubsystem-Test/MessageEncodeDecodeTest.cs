3451711a3c0c7c9ff1dab707d8ae69d0c6f64179
Jason Bingham <jbingham408@gmail.com>
2017-10-31 15:13:14 -0600

    More work on tests

--- Palette/CommunicationSubsystem-Test/MessageEncodeDecodeTest.cs
+++ Palette/CommunicationSubsystem-Test/MessageEncodeDecodeTest.cs
@@ -12,23 +12,28 @@ namespace UnitTestProject1
         [TestMethod]
         public void BrushStrokeMessage()
         {
-            BrushStrokeMessage message = new BrushStrokeMessage();
-            message.MessageNumber = new Tuple<short, short>(1, 1);
-            message.ConversationId = new Tuple<short, short>(2, 1);
-            message.CanvasId = 1;
+            var message = new BrushStrokeMessage()
+            {
+                MessageNumber = new Tuple<short, short>(1, 1),
+                ConversationId = new Tuple<short, short>(2, 1),
+                CanvasId = 1,
+                BrushType = "Solid",
+                Points = new List<Tuple<int, int>>()
+            };
+
             message.Points.Add(new Tuple<int, int>(1200, 1300));
             message.Points.Add(new Tuple<int, int>(1201, 1302));
             message.Points.Add(new Tuple<int, int>(1203, 1305));
             message.Points.Add(new Tuple<int, int>(1205, 1306));
             message.Points.Add(new Tuple<int, int>(3, 34));
             message.Points.Add(new Tuple<int, int>(10, 13));
-            message.BrushType = "Solid";
 
             byte[] bytes = message.Encode();
 
             Assert.AreEqual(123, bytes[0]);
+            Assert.AreEqual(34, bytes[1]);
 
-            Message message2 = Message.Decode(bytes);
+            var message2 = Message.Decode(bytes);
 
             Assert.AreEqual(message.MessageNumber, (message2 as BrushStrokeMessage).MessageNumber);
             Assert.AreEqual(message.ConversationId, (message2 as BrushStrokeMessage).ConversationId);
