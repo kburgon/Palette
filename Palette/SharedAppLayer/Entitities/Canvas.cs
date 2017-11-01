edc44a49b1eb5421789ed37a10d7a144aa953390
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2017-10-31 19:03:12 -0600

    Adds SharedAppLayer project

---
+++ Palette/SharedAppLayer/Entitities/Canvas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedAppLayer.Entitities
+{
+    // TODO: This should probably be moved to some common sublayer
+    public class Canvas
+    {
+        public uint CanvasId { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public IEnumerable<BrushStroke> BrushStrokes { get; set; }
+    }
+}
