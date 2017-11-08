b7515d5c8303bac90d54dfefa7de06203ab76018
Jason Bingham <jbingham408@gmail.com>
2017-11-07 17:26:11 -0700

    Minor edit to udp comm

--- Palette/CommunicationSubsystem/UDPCommunicator.cs
+++ Palette/CommunicationSubsystem/UDPCommunicator.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Net;
 using System.Net.Sockets;
-using System.Threading.Tasks;
-using System.IO;
 using System.Threading;
 using Messages;
 using log4net;
