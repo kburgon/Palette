cb9896549359dc5d4f770f509000403f910fccc5
Jason Bingham <jbingham408@gmail.com>
2017-11-17 18:44:06 -0700

    Created a new folder for conversations in the AuthManagerAppLayer and created the AuthManagerConversation Factory.

---
+++ Palette/AuthManagerAppLayer/AuthManagerConversationFactory.cs
@@ -0,0 +1,17 @@
+﻿using CommunicationSubsystem.ConversationFactories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthManagerAppLayer
+{
+    class AuthManagerConversationFactory : ConversationFactory
+    {
+        public override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
