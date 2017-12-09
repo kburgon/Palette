d143d99eeccd3f3edeeaadf170201d9eca0bd999
Jason Bingham <jbingham408@gmail.com>
2017-12-09 14:39:42 -0700

    Added Display tests, lots of work with display conversations, Successfully can register a diplay on a local network.

---
+++ Palette/DisplayTests/DisplayTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DisplayManagerAppLayer;
+using DisplayAppLayer;
+using System.Net;
+
+namespace DisplayTests
+{
+    [TestClass]
+    public class DisplayTest
+    {
+        [TestMethod]
+        public void UpdateDisplayManagerEndPointTest()
+        {
+            Display display = new Display();
+
+            display.UpdateDisplayManagerAddress("127.0.1.1");
+
+            Assert.AreNotEqual(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12250), display.DisplayManagerEP);
+            Assert.AreEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12250), display.DisplayManagerEP);
+
+            display.UpdateDisplayManagerPort(12342);
+            Assert.AreNotEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12250), display.DisplayManagerEP);
+            Assert.AreEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12342), display.DisplayManagerEP);
+        }
+
+        [TestMethod]
+        public void UpdateCanvasManagerEndPointTest()
+        {
+            Display display = new Display();
+
+            display.UpdateCanvasManagerAddress("127.0.1.1");
+
+            Assert.AreNotEqual(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345), display.CanvasManagerEP);
+            Assert.AreEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12345), display.CanvasManagerEP);
+
+            display.UpdateCanvasManagerPort(12342);
+            Assert.AreNotEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12345), display.CanvasManagerEP);
+            Assert.AreEqual(new IPEndPoint(IPAddress.Parse("127.0.1.1"), 12342), display.CanvasManagerEP);
+        }
+    }
+}
---
+++ Palette/DisplayTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DisplayTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DisplayTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03773c62-be27-4f9f-9c5c-479e513ed97c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
