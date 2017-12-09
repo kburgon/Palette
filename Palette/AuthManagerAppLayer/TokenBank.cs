0d402264eef6519f049ad03b293ab60c73e55009
Kevin Burgon <kevin.burgon@aggiemail.usu.edu>
2017-12-09 14:37:10 -0700

    Adds encryption to the token bank

--- Palette/AuthManagerAppLayer/TokenBank.cs
+++ Palette/AuthManagerAppLayer/TokenBank.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Linq;
+using CommunicationSubsystem.Security;
+using log4net;
 
 namespace AuthManagerAppLayer
 {
     public class TokenBank
     {
+        public string PublicKey { get; }
         private ConcurrentBag<Guid> Tokens { get; }
+        private KeyBank Keys { get; }
+        private ILog Logger = LogManager.GetLogger(typeof(TokenBank));
         private static object _bankLock = new object();
         private static TokenBank _bankInstance;
 
         private TokenBank()
         {
             Tokens = new ConcurrentBag<Guid>();
+            Keys = new KeyBank();
+            PublicKey = Keys.GeneratePublicKey();
             _bankInstance = this;
         }
 
@@ -26,6 +33,20 @@ namespace AuthManagerAppLayer
                 }
 
                 return _bankInstance;
+            }
+        }
+
+        public bool EncryptedTokenExists(byte[] encryptedToken)
+        {
+            try
+            {
+                var tokenBytes = Keys.Decrypt(encryptedToken);
+                return TokenExists(new Guid(tokenBytes));
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+                return false;
             }
         }
 
